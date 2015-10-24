664c81fee422389ab844a723c319743d75049c8f
Louis Cognault <louis.cognault@gmail.com>
2015-10-24 17:02:06 +0200

    Deleted unneeded file

--- app/src/main/java/xyz/louiscad/popularmovies/prefs/MovieListPrefs.java
+++
@@ -1,9 +0,0 @@
-package xyz.louiscad.popularmovies.prefs;
-
-import org.androidannotations.annotations.sharedpreferences.DefaultString;
-import org.androidannotations.annotations.sharedpreferences.SharedPref;
-
-/**
- * Created by Louis Cognault on 23/10/15.
- */
-
