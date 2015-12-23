1fdd83e65303e864125caf66245774a412dabf21
Louis Cognault <louis.cognault@gmail.com>
2015-12-23 18:28:40 +0100

    Fixed false positive two pane bug on tablets which caused crash

--- app/src/main/java/xyz/louiscad/popularmovies/ui/activity/MainActivity.java
+++ app/src/main/java/xyz/louiscad/popularmovies/ui/activity/MainActivity.java
@@ -1,12 +1,15 @@
 package xyz.louiscad.popularmovies.ui.activity;
 
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 import org.androidannotations.annotations.AfterViews;
 import org.androidannotations.annotations.EActivity;
 import org.androidannotations.annotations.FragmentById;
 import org.androidannotations.annotations.OptionsItem;
 import org.androidannotations.annotations.OptionsMenu;
+import org.androidannotations.annotations.ViewById;
 
 import trikita.log.Log;
 import xyz.louiscad.popularmovies.R;
@@ -25,6 +28,9 @@ public class MainActivity extends AppCompatActivity implements MovieListItem.Cli
     @FragmentById(R.id.detailFragment)
     MovieDetailFragment mMovieDetailFragment;
 
+    @ViewById
+    View twoPaneContainer;
+
     /**
      * Whether or not the activity is in two-pane mode, i.e. running on a tablet
      * device.
@@ -33,12 +39,29 @@ public class MainActivity extends AppCompatActivity implements MovieListItem.Cli
 
     @AfterViews
     void init() {
-        mTwoPane = mMovieDetailFragment != null;
+        //mTwoPane = mMovieDetailFragment != null; //
+        mTwoPane = twoPaneContainer != null;
+        Log.d("mTwoPane %b", mTwoPane);
+        Log.d("moviesFragment id: " + mMoviesFragment.getId());
+        Log.d("detailsFragment id: " + mMovieDetailFragment.getId());
     }
 
     @OptionsItem
     void actionSettingsSelected() {
         Log.i("Settings clicked");
+    }
+
+        //Log.i("Fragment attached with id: " + fragment.getId());
+    @Override
+    public void onAttachFragment(Fragment fragment) {
+        this.mMoviesFragment = findSupportFragmentById(R.id.moviesFragment, fragment);
+        this.mMovieDetailFragment = findSupportFragmentById(R.id.detailFragment, fragment);
+    }
+
+    @SuppressWarnings("unchecked cast")
+    private <F extends android.support.v4.app.Fragment> F findSupportFragmentById(int id, Fragment fragment) {
+        boolean isExpected = id == fragment.getId();
+        return (isExpected ? (F) fragment : null);
     }
 
     @Override
